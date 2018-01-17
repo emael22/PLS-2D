b323179fa396a0d84e3eb81be9c321a527488fc4
TOUDERT Malik <malik.toudert@djit.fr>
2018-01-17 12:10:35 +0100

    start

---
+++ Assets/player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+    public float speed = 10f;
+
+    private Rigidbody2D rb;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal")* Time.fixedDeltaTime* speed;
+
+        rb.MovePosition(rb.position + Vector2.right * x);
+    }
+    
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
